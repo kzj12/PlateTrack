2e44038f1e38a2a67c2f132db71d8e4fa8108a75
Unknown <kzj12@live.missouristate.edu>
2017-09-25 18:22:25 -0500

    test

--- app/src/main/java/com/example/america/platetrack2/Route.java
+++ app/src/main/java/com/example/america/platetrack2/Route.java
@@ -8,10 +8,12 @@ import java.util.ArrayList;
  * Created by matth on 5/7/2017.
  */
 
+//testnumber2
 public class Route {
     public ArrayList<PlateCapture> points;
 
     public boolean isChecked = false;
+    public boolean testing = true;
 
     public ArrayList<PlateCapture> getPoints() {
         return points;
